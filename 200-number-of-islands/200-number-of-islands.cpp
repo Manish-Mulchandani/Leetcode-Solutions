ef4319813bae8bcb1d031586854b735fc424b287
Manish-Mulchandani <56696028+Manish-Mulchandani@users.noreply.github.com>
2022-07-23 04:52:00 +0530

    Time: 60 ms (41.13%), Space: 18.2 MB (13.03%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,7 +1,45 @@
 class Solution {
 public:
     
-    void dfs(vector<vector<char>> &grid, int i, int j)
+    // M-II BFS
+    int numIslands(vector<vector<char>>& grid) {
+        int totalislands=0;
+        int offsets[]={0,1,0,-1,0};
+        int a,b;
+        for(int i=0; i<grid.size(); i++)
+        {
+            for(int j=0; j<grid[0].size(); j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    totalislands++;
+                    grid[i][j]='2';
+                    queue<pair<int,int>> q;
+                    q.push({i,j});
+                    while(!q.empty())
+                    {
+                        auto p=q.front();
+                        q.pop();
+                        for(int k=0; k<4; k++)
+                        {
+                            a=p.first+offsets[k];
+                            b=p.second+offsets[k+1];
+                            if(a>=0 && a<grid.size() && b>=0 && b<grid[0].size() && grid[a][b]=='1')
+                            {
+                                grid[a][b]='2';
+                                q.push({a,b});
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
+        return totalislands;
+    }
+    
+    // M-I DFS without a vis array
+    /*void dfs(vector<vector<char>> &grid, int i, int j)
     {
         if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j]!='1')
             return ;
@@ -28,5 +66,5 @@ public:
             }
         }
         return totalislands;
-    }
+    }*/
 };
