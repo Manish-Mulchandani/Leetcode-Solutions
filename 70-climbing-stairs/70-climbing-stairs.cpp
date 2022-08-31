cd335dd3bb4f3c0bbcebb65c3ac4f8d94df77a5c
Manish-Mulchandani <56696028+Manish-Mulchandani@users.noreply.github.com>
2022-08-31 19:41:34 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (99.97%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     
-    // M-III Tabulation
+    /* M-IV Space Optimization */
+    int climbStairs(int n)
+    {
+        int t1=0,t2=1,t3=1;
+        for(int i=1; i<=n; i++)
+        {
+            if(i>1)
+                t3=t1+t2;
+            else 
+                t3=t2;
+            t1=t2;
+            t2=t3;
+        }
+        return t3;
+    }
+    
+    /* M-III Tabulation
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
         dp[0]=1;
@@ -12,7 +28,7 @@ public:
                 dp[i]+=dp[i-2];
         }
         return dp[n];
-    }
+    }*/
     
     /*M-II Memoization  TC-O(N) SC-O(2N)
     int f(int n, vector<int> &dp)
