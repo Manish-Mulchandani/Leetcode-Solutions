f157f55b41b4d77b59ee037346d056303e72a22b
Manish-Mulchandani <56696028+Manish-Mulchandani@users.noreply.github.com>
2022-09-02 23:26:27 +0530

    Time: 9 ms (7.47%), Space: 7.9 MB (6.24%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    /*M-V Very optimized
+    int rob(vector<int>& nums) {
+        int a=0,b=0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(i%2)
+            {
+                a=max(a+nums[i], b);
+            }
+            else
+            {
+                b=max(b+nums[i], a);
+            }
+        }
+        return max(a,b);
+    }*/
+    
+    int f(vector<int> &nums, int n,vector<int> &dp)
+    {
+        if(n<0)
+            return 0;
+        if(dp[n]!=-1)
+            return dp[n];
+        int take=f(nums,n-2,dp)+nums[n];
+        int nottake=f(nums,n-1,dp);
+        return dp[n]=max(take,nottake);
+    }
+    
+    int rob(vector<int>& nums){
+        vector<int> dp(nums.size(),-1);
+        f(nums,nums.size()-1,dp);
+        return dp[nums.size()-1];
+    }
+    
+    /* M-I Recursion
+    int f(vector<int> &nums, int n)
+    {
+        if(n<0)
+            return 0;
+        int take=nums[n]+f(nums,n-2);
+        int nottake=f(nums,n-1);
+        return max(take,nottake);
+    }
+    int rob(vector<int>& nums){
+        return f(nums,nums.size()-1);
+    }*/
+};
