9ee0699d4ab6f3de6ac78f43974d24dc07b4d7bc
Manish-Mulchandani <56696028+Manish-Mulchandani@users.noreply.github.com>
2022-09-22 12:28:38 +0530

    Time: 15 ms (85.69%), Space: 11.5 MB (28.33%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -2,6 +2,26 @@ class Solution {
 public:
     string reverseWords(string s) {
         string word="";
+        string ans="";
+        for(auto &i:s)
+        {
+            if(i!=' ')
+                word.push_back(i);
+            else 
+            {
+                reverse(word.begin(), word.end());
+                ans.append(word);
+                word="";
+                ans.push_back(' ');
+            }
+        }
+        reverse(word.begin(), word.end());
+        ans.append(word);
+        return ans;
+    }
+    /*M-I
+    string reverseWords(string s) {
+        string word="";
         int j=-1,l=0;
         for(int i=0; i<s.length(); i++)
         {
@@ -23,5 +43,5 @@ public:
         for(int k=j; k<s.length(); k++, l++)
             s[k]=word[l];
         return s;
-    }
+    }*/
 };
